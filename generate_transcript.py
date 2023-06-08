0784fa8790118833afc2b146db8ebd22eb78d795
Tahir Fayyaz <tahir.fayyaz@databricks.com>
2023-06-08 23:54:40 +0100

    added code to generate transcripts

--- generate_transcript.py
+++ generate_transcript.py
@@ -1,5 +1,7 @@
 import json
+import os
 from datetime import timedelta
+from youtube_transcript_api import YouTubeTranscriptApi
 
 def convert_to_hms(seconds):
     td = timedelta(seconds=float(seconds))
@@ -7,15 +9,41 @@ def convert_to_hms(seconds):
     hours, minutes = divmod(minutes, 60)
     return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
 
-# Read the JSON file
-with open('./static/data/transcripts/1.json', 'r') as file:
-    video = json.load(file)
+# Load the videos
+with open('static/data/videos.json', 'r') as f:
+    videos = json.load(f)
 
-# Add the 'time' key to each transcript entry
-for transcript in video['video_transcript']:
-    transcript['time'] = convert_to_hms(transcript['start'])
+# Loop through each video
+for video in videos:
+    video_id = video["video_id"]
+    video_name = video["video_name"]
 
-# Write the modified data back to the JSON file
-with open('./static/data/transcripts/1.json', 'w') as file:
-    print("modified data back to the JSON file")
-    json.dump(video, file, indent=4)
+    # Prepare the path to the transcript file
+    transcript_file_path = f'./static/data/transcripts/{video_id}.json'
+    
+    # Skip if the transcript file already exists
+    if os.path.isfile(transcript_file_path):
+        print(f"Transcript for {video_name} already exists. Skipping...")
+        continue
+
+    # Transcript retrieval
+    video_transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+    # Add the 'time' key to each transcript entry
+    for t in video_transcript:
+        t['time'] = convert_to_hms(t['start'])
+
+    # Prepare video data
+    video_data = {
+        "video_id": video_id,
+        "video_name": video_name,
+        "video_thumb_url": video["video_thumb_url"],
+        "video_date": video["video_date"],
+        "video_short_description": video["video_short_description"],
+        "video_transcript": video_transcript
+    }
+
+    # Write the modified data back to the JSON file
+    with open(transcript_file_path, 'w') as file:
+        print(f"Writing transcript for {video_name} to JSON file")
+        json.dump(video_data, file, indent=4)
